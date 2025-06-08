1b1b8317833af6d1a15b2afe0ac2cf9fc17b4d45
Dmitrii Palkin <dvpalkin@gmail.com>
2025-06-09 00:40:18 +0300

    1st commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,20 +1,80 @@
+// Package daysteps implements calculating information about user day activity.
 package daysteps
 
 import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 const (
-	// Длина одного шага в метрах
-	stepLength = 0.65
-	// Количество метров в одном километре
-	mInKm = 1000
+	stepLength = 0.65 // Length of one step in meters
+
+	mInKm = 1000 // Meters in one kilometer
 )
 
+// Error vars definition.
+var (
+	ErrWrongData = errors.New("wrong data") // Wrong data format error
+)
+
+// parcePackage converts string containing steps and time to int and time.Duration formats.
+// Returns int steps, time.Duration Duration or error.
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+
+	// split string
+
+	splittedData := strings.Split(data, ",")
+	if len(splittedData) != 2 {
+		return 0, 0, ErrWrongData
+	}
+
+	// convert data
+
+	steps, err := strconv.Atoi(splittedData[0])
+
+	if err != nil || steps <= 0 {
+		return 0, 0, errors.Join(ErrWrongData, err)
+	}
+
+	walkDuration, err := time.ParseDuration(splittedData[1])
+
+	if err != nil || walkDuration <= 0 {
+		return 0, 0, errors.Join(ErrWrongData, err)
+	}
+
+	return steps, walkDuration, nil
 }
 
+// DayActionInfo calculate and return information in text format about day activity of user.
+// Output string countains information about steps count, passed distance in km and burned kCals.
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+
+	steps, walkDuration, err := parsePackage(data)
+
+	if err != nil {
+		log.Print(err.Error())
+		return ""
+	}
+
+	if steps <= 0 {
+		return ""
+	}
+
+	var distanseKm float64 = float64(steps) * stepLength / mInKm
+
+	kCal, err := spentcalories.WalkingSpentCalories(steps, weight, height, walkDuration)
+
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", steps, distanseKm, kCal)
+
 }
--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -1,38 +1,132 @@
+// Package spentcalories implements functions for calculating calories based on training and user parameters.
+// Also contains functions collecting training report.
 package spentcalories
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
-// Основные константы, необходимые для расчетов.
+// Basic constants for calculations.
 const (
-	lenStep                    = 0.65 // средняя длина шага.
-	mInKm                      = 1000 // количество метров в километре.
-	minInH                     = 60   // количество минут в часе.
-	stepLengthCoefficient      = 0.45 // коэффициент для расчета длины шага на основе роста.
-	walkingCaloriesCoefficient = 0.5  // коэффициент для расчета калорий при ходьбе
+	lenStep                    = 0.65 // average step length.
+	mInKm                      = 1000 // number of meters in a kilometer.
+	minInH                     = 60   // number of minutes in an hour.
+	stepLengthCoefficient      = 0.45 // coefficient for calculating step length based on height.
+	walkingCaloriesCoefficient = 0.5  // coefficient for calculating calories from walking
 )
 
+// Error vars definition.
+
+var (
+	ErrUnknownTrainType = errors.New("неизвестный тип тренировки") // Unknown Training Type error
+	ErrIncorrectParams  = errors.New("incorrect input parameters") // Incorrect input parameters error
+	ErrWrongData        = errors.New("wrong data format")          // Wrong data format error
+)
+
+// parseTraining parces training data string and returns steps count, activity type and training duration.
 func parseTraining(data string) (int, string, time.Duration, error) {
-	// TODO: реализовать функцию
+
+	splittedData := strings.Split(data, ",")
+
+	if len(splittedData) != 3 {
+		return 0, "", 0, ErrWrongData
+	}
+
+	steps, err := strconv.Atoi(splittedData[0])
+
+	if err != nil || steps <= 0 {
+		return 0, "", 0, errors.Join(ErrWrongData, err)
+	}
+
+	activity := splittedData[1]
+
+	if len(activity) == 0 {
+		return 0, "", 0, ErrUnknownTrainType
+	}
+
+	duration, err := time.ParseDuration(splittedData[2])
+
+	if err != nil || duration <= 0 {
+		return 0, "", 0, errors.Join(ErrWrongData, err)
+	}
+
+	return steps, activity, duration, nil
+
 }
 
+// distance calculates the distance based on step count and user height.
 func distance(steps int, height float64) float64 {
-	// TODO: реализовать функцию
+
+	return height * stepLengthCoefficient * float64(steps) / mInKm
 }
 
+// meanSpeed calculates the meanSpeed based on step count, user height and activity duration.
 func meanSpeed(steps int, height float64, duration time.Duration) float64 {
-	// TODO: реализовать функцию
+
+	if duration <= 0 {
+		return 0
+	}
+
+	return distance(steps, height) / duration.Hours()
 }
 
+// TrainingInfo function make a training report and returns it in string format.
 func TrainingInfo(data string, weight, height float64) (string, error) {
-	// TODO: реализовать функцию
+
+	steps, activityType, trainingDuration, err := parseTraining(data)
+
+	if err != nil {
+		return "", err
+	}
+
+	var spentCalories float64
+
+	switch activityType {
+	case "Бег":
+		spentCalories, err = RunningSpentCalories(steps, weight, height, trainingDuration)
+	case "Ходьба":
+		spentCalories, err = WalkingSpentCalories(steps, weight, height, trainingDuration)
+	default:
+		return "", ErrUnknownTrainType
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	trainingResult := fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\n", activityType, trainingDuration.Hours())
+	trainingResult += fmt.Sprintf("Дистанция: %.2f км.\n", distance(steps, height))
+	trainingResult += fmt.Sprintf("Скорость: %.2f км/ч\n", meanSpeed(steps, height, trainingDuration))
+	trainingResult += fmt.Sprintf("Сожгли калорий: %.2f\n", spentCalories)
+
+	return trainingResult, nil
+
 }
 
+// RunningSpentCalories calculates spent calories for running activity based on steps count, user weight and height and activity duration.
+// Returns 0 and "Incorrect input parameters" error if any parameter == 0.
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+
+	if steps <= 0 || weight <= 0 || height <= 0 || duration <= 0 {
+		return 0, ErrIncorrectParams
+	}
+
+	return weight * meanSpeed(steps, height, duration) * duration.Minutes() / minInH, nil
+
 }
 
+// WalkingSpentCalories calculates spent calories for walking activity based on steps count, user weight and height and activity duration.
+// Returns 0 and "Incorrect input parameters" error if any parameter == 0.
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+
+	if steps <= 0 || weight <= 0 || height <= 0 || duration <= 0 {
+		return 0, ErrIncorrectParams
+	}
+
+	return weight * meanSpeed(steps, height, duration) * duration.Minutes() / minInH * walkingCaloriesCoefficient, nil
+
 }
